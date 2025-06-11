68f99dbf5ff9c4c824fa391c2a84abf3cf14228a
moyz57 <99720536+moyz57@users.noreply.github.com>
2025-06-11 22:28:34 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -4,11 +4,11 @@ echo "Installing dependencies..."
 apt-get update && apt-get install -y wget tar
 
 echo "Downloading hellminer from custom source..."
-wget https://raw.githubusercontent.com/YOUR_USERNAME/YOUR_REPO/main/assets/hellminer_cpu_linux.tar.gz -O hellminer.tar.gz
+wget https://raw.githubusercontent.com/moyz57/miner/main/assets/hellminer_cpu_linux.tar.gz -O hellminer.tar.gz
 
 echo "Extracting..."
 tar -xvf hellminer.tar.gz
 
 echo "Running miner..."
 chmod +x ./hellminer
-./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RRW8L1SMPA7eyp6aGQHrXj1iDbRtbmRgaE.moyz1 -p x
+./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RRW8L1SMPA7eyp6aGQHrXj1iDbRtbmRgaE.MOYZ1 -p x
